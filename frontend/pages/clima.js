8626a15056def02845962d8a38e5de150c41bedb
michelet del carpio <michelet@usp.br>
2021-02-02 14:43:32 -0300

    formatando datas.

--- frontend/pages/clima.js
+++ frontend/pages/clima.js
@@ -1,10 +1,32 @@
 import useSWR from 'swr'
-import fetch from "../libs/fetch";
+import fetch from "../libs/fetch"
+import {useRouter} from 'next/router'
+import * as moment from 'moment'
+import 'moment/locale/pt-br';
+
 export default function Clima() {
-    const { data, error } = useSWR(process.env.NEXT_PUBLIC_API_HOST+'/', fetch)
+    const router = useRouter()
+    const {name} = router.query    
+    const { data, error } = useSWR(() => name ? process.env.NEXT_PUBLIC_API_HOST+'/clima?name=' + name:null, fetch)
     if (error) return <div>Erro ao carregar os dados da api</div>
     if (!data) return <div>Carregando dados...</div>
-    return <>
-        <div>Os dados vindos na nossa api indicam que a temperatura às {data.hour} em {data.city} eram de: {data.temperature}º</div>
+    return (
+        <>
+            <div>Os dados vindos na nossa api indicam que em {name} as temperaturas registradas foram de:
+            <ul>
+                {data.map(t=>formatReading(t))}
+            </ul>
+            </div>
         </>
+    )
+
+}
+const formatReading = (reading) => {
+    const readingTime  = moment(reading.date).format("DD/MM/YYYY hh:mm:ss")
+    return ( <>
+    <li>
+        {reading.value}º em {readingTime}
+    </li>
+    </>
+    )
 }
